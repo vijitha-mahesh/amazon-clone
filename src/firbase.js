5450567477808015c3ba6761eb65f24dbd1b274b
vijitha mahesh <185044c@uom.lk>
2021-11-05 14:38:25 +0530

    initial commit

---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCtjMhIDGOy6xV5IVtAjIMJvGSXcOPo4SQ",
+    authDomain: "challenge-fa690.firebaseapp.com",
+    projectId: "challenge-fa690",
+    storageBucket: "challenge-fa690.appspot.com",
+    messagingSenderId: "317230096730",
+    appId: "1:317230096730:web:3ee896965149fbee319363",
+    measurementId: "G-1MNCH66CTC"
+  };
